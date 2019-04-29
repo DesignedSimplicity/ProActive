90a9153727a38782c684ee8f58639ada74f5a720
Kevin <Kevin@k2>
2019-04-29 17:34:31 +1200

    Initial structure created

---
+++ ProActive.Lib/ParseVideo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NReco.VideoInfo;
+
+namespace ProActive.Lib
+{
+    public class ParseVideo
+    {
+        public void Test()
+        {
+            var videoUri = @"C:\test\test.mp4";
+            var probe = new FFProbe();
+            var info = probe.GetMediaInfo(videoUri);
+            Console.WriteLine(info.FormatName);
+            Console.WriteLine(info.Duration);
+
+        }
+    }
+}
---
+++ ProActive.Test/ParseVideoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProActive.Test
+{
+    [TestClass]
+    public class ParseVideoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
